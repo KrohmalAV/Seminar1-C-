4b0a804135e7b71e23cc119c653cac7495548c87
KrohmalAV <Anton.avk@yandex.ru>
2022-11-20 12:41:57 +0300

    Внесены новые текстовые изменения в задачу по квадрату

--- Sem1.1/Program.cs
+++ Sem1.1/Program.cs
@@ -1,11 +1,13 @@
-﻿int a = Convert.ToInt32(Console.ReadLine());
+﻿Console.Clear();
+Console.Write("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-
 if(a == b * b)
 {
     Console.WriteLine($"{a} является квадратом {b}");
 }
 else
 {
-    Console.WriteLine("{a} не является квадратом {b}");
+    Console.WriteLine($"{a} не является квадратом {b}");
 }
