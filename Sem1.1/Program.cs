d21b5203cfdfe51eb332848835e0bb5a1182eb3f
KrohmalAV <Anton.avk@yandex.ru>
2022-11-20 11:49:29 +0300

    Задача по квадрату

--- Sem1.1/Program.cs
+++ Sem1.1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int b2 = b * b;
+if(a == b2)
+{
+    Console.WriteLine($"{a} является квадратом {b}");
+}
+else
+{
+    Console.WriteLine("{a} не является квадратом {b}");
+}
