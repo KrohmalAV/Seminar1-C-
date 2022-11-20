02e2f9839d6dd2d63c96105fcba6374fafe63739
KrohmalAV <Anton.avk@yandex.ru>
2022-11-20 15:15:05 +0300

    Внесены изменения в задачу по промежуток

--- Sem1.3/Program.cs
+++ Sem1.3/Program.cs
@@ -1,8 +1,20 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Clear();
+Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
  int i = 0 - a;
- while(i <= a)
+ if(a >= 0)
  {
-    Console.Write($"{i} ");
-    i++;
+   while(i <= a)
+   {
+      Console.Write($"{i} ");
+      i++;
+   }
+ }
+ else
+ {
+   while(i >= a)
+   {
+      Console.Write($"{i} ");
+      i--;
+   }
  }
