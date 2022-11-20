3658649bc83871ebf4ab41b7fa9c2fb2e91574e0
KrohmalAV <Anton.avk@yandex.ru>
2022-11-20 12:46:13 +0300

    Внесены новые текстовые изменения в задачу по дням недели

--- Sem1.2/Program.cs
+++ Sem1.2/Program.cs
@@ -1,19 +1,35 @@
-﻿Console.WriteLine("Введите номер дня недели: ");
+﻿Console.Clear();
+Console.Write("Введите номер дня недели (от 1 до 7): ");
 int a = Convert.ToInt32(Console.ReadLine());
-
 if(a == 1)
-{Console.WriteLine($"{a} - понедельник");}
+{
+    Console.WriteLine($"{a} день недели - понедельник");
+}
 else if(a == 2)
-{Console.WriteLine($"{a} - вторник");}
+{
+    Console.WriteLine($"{a} день недели - вторник");
+}
 else if(a == 3)
-{Console.WriteLine($"{a} - среда");}
+{
+    Console.WriteLine($"{a} день недели - среда");
+}
 else if(a == 4)
-{Console.WriteLine($"{a} - четверг");}
+{
+    Console.WriteLine($"{a} день недели - четверг");
+}
 else if(a == 5)
-{Console.WriteLine($"{a} - пятница");}
+{
+    Console.WriteLine($"{a} день недели - пятница");
+}
 else if(a == 6)
-{Console.WriteLine($"{a} - суббота");}
+{
+    Console.WriteLine($"{a} день недели - суббота");
+}
 else if(a == 7)
-{Console.WriteLine($"{a} - воскресение");}
+{
+    Console.WriteLine($"{a} день недели - воскресение");
+}
 else
-{Console.WriteLine("такого дня недели не существует");}
+{
+    Console.WriteLine("Такого дня недели не существует. Необходимо было ввести число от 1 до 7...");
+}
