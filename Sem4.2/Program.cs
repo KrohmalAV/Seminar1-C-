75dc851b330b7f395d0ed64eb83b884d62679319
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 11:13:49 +0300

    Поиск количества цифр в числе

---
+++ Sem4.2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число
+и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+string A = Console.ReadLine();
+int length = A.Length;
+Console.WriteLine($"В числе {A} - {length} цифр");
