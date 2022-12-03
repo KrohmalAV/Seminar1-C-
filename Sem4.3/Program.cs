17e3a71f7b41f5c234fed7499e5526b26d48494f
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 11:17:47 +0300

    Поиск произмедения чисел из диапазона

---
+++ Sem4.3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N
+и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int pro = 1;
+for(int i = 1; i <= N; i++) pro = pro * i;
+Console.WriteLine(pro);
