80e1bb4606a5c54ca077ce16e015f659ad3d7457
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 23:44:57 +0300

    Произведение чисел из диапазона - добавлено решение через метод

--- Sem4.3/Program.cs
+++ Sem4.3/Program.cs
@@ -5,9 +5,24 @@
 5 -> 120
 */
 
+// Через метод
+int Multi(int N)
+{
+    int pro = 1;
+    for(int i = 1; i <= N; i++) pro = pro * i;
+    return pro;
+}
+
 Console.Clear();
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int pro = 1;
-for(int i = 1; i <= N; i++) pro = pro * i;
-Console.WriteLine(pro);
+Console.WriteLine(Multi(N));
+
+
+// Без метода
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int pro = 1;
+// for(int i = 1; i <= N; i++) pro = pro * i;
+// Console.WriteLine(pro);
