7eda28d6db6268ddcfc3aef0a6bbc7c98dd00e29
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 11:34:27 +0300

    Вывод массива из 8 элементов из 0 и 1

---
+++ Sem4.4/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+// Console.Clear();
+// void FillArray(int[]array)
+// {
+//     int i = 0;
+//     while(i < 8)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//         i++;
+//     }
+// }
+// void PrintArray(int[]col)
+// {
+//     int position = 0;
+//     while(position < 8)
+//     {
+//         Console.Write($"{col[position]} ");
+//         position++;
+//     }
+// }
+// int[] array = new int[8];
+// FillArray(array);
+// PrintArray(array);
+
+// Без функций
+Console.Clear();
+int[] array = new int[8];
+for(int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write($"{array[i]} ");
+}
