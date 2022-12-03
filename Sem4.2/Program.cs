3c39f246baa039f5da65458da61228193cea3ba8
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 23:51:28 +0300

    Подсчет количества цифр в числе - добавлено решение через метод

--- Sem4.2/Program.cs
+++ Sem4.2/Program.cs
@@ -6,6 +6,24 @@
 89126 -> 5
 */
 
+// Решение через метод
+int Count(int A)
+{
+    int count = 0;
+    while(A > 0)
+    {
+        A /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"В числе {A} - {Count(A)} цифр");
+
+
 // Решение через строки
 // Console.Clear();
 // Console.Write("Введите число: ");
@@ -13,15 +31,15 @@
 // int length = A.Length;
 // Console.WriteLine($"В числе {A} - {length} цифр");
 
-// Решение без использования строк
-Console.Clear();
-Console.Write("Введите число: ");
-int A = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int a = A;
-while(a > 0)
-{
-    a /= 10;
-    count++;
-}
-Console.WriteLine($"В числе {A} - {count} цифр");
+// Решение без использования строк и метода
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int a = A;
+// while(a > 0)
+// {
+//     a /= 10;
+//     count++;
+// }
+// Console.WriteLine($"В числе {A} - {count} цифр");
