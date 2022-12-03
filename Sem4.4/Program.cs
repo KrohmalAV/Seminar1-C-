ade2554111d0c4833d65d1a52e0a17d2bae5c718
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 12:13:53 +0300

    Вывод массива из 8 элементов из 0 и 1 - добавлено решение без метода, в методи оптимизирован цикл

--- Sem4.4/Program.cs
+++ Sem4.4/Program.cs
@@ -4,34 +4,24 @@
 [1,0,1,1,0,1,0,0]
 */
 
-// Console.Clear();
-// void FillArray(int[]array)
-// {
-//     int i = 0;
-//     while(i < 8)
-//     {
-//         array[i] = new Random().Next(0, 2);
-//         i++;
-//     }
-// }
-// void PrintArray(int[]col)
-// {
-//     int position = 0;
-//     while(position < 8)
-//     {
-//         Console.Write($"{col[position]} ");
-//         position++;
-//     }
-// }
-// int[] array = new int[8];
-// FillArray(array);
-// PrintArray(array);
+Console.Clear();
+void FillArray(int[]array)
+{
+    for(int i = 0; i < 8; i++) array[i] = new Random().Next(0, 2);
+}
+void PrintArray(int[]array)
+{
+    for(int i = 0; i < 8; i++) Console.Write($"{array[i]} ");
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
 
 // Без функций
-Console.Clear();
-int[] array = new int[8];
-for(int i = 0; i < 8; i++)
-{
-    array[i] = new Random().Next(0, 2);
-    Console.Write($"{array[i]} ");
-}
+// Console.Clear();
+// int[] array = new int[8];
+// for(int i = 0; i < 8; i++)
+// {
+//     array[i] = new Random().Next(0, 2);
+//     Console.Write($"{array[i]} ");
+// }
