85be2b559ea3a0f1ccbec6709e5be31603f0f6ae
KrohmalAV <Anton.avk@yandex.ru>
2022-12-10 14:34:20 +0300

    Проверка существования треугольника - добавлен способ ввода данных через split

--- Sem6.2/Program.cs
+++ Sem6.2/Program.cs
@@ -5,6 +5,7 @@
 суммы двух других сторон
 */
 
+// Ввод длин сторон по очереди
 Console.Clear();
 Console.Write("Введите длинну первой стороны треугольника: ");
 double a = Convert.ToDouble(Console.ReadLine());
@@ -17,3 +18,19 @@ if(a+b > c && b+c > a && c+a > b && a > 0 && b > 0 && c > 0)
     Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} существует");
 else 
     Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} не существует");
+
+// Ввод длин сторон сразу через пробел
+Console.Clear();
+Console.Write("Введите длины сторон треугольника через пробел: ");
+string a = Console.ReadLine();
+string[] array = a.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+double[] arr = array.Select(double.Parse).ToArray();
+if(arr[0]+arr[1] > arr[2]
+    && arr[1]+arr[2] > arr[0]
+    && arr[0]+arr[2] > arr[1]
+    && arr[0] > 0
+    && arr[1] > 0
+    && arr[2] > 0)
+    Console.WriteLine("Такой треугольник существует");
+else 
+    Console.WriteLine("Такого треугольник не существует");
