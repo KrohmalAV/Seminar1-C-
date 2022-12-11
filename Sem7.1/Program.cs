522422aeceffdd11a0d3cbe72991263aa7139731
KrohmalAV <Anton.avk@yandex.ru>
2022-12-11 10:52:43 +0300

    Создание двумерного массива

---
+++ Sem7.1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+
+*/
+
+int[,] FillDoubleArray(int row, int column, int min, int max)  // Заполнение двумерного массива (рандомно)
+{
+    int[,] array = new int[row, column];
+    for(int i = 0; i < row; i++) 
+        for(int j = 0; j < column; j++)
+            array[i,j] = new Random().Next(min, max);
+    return array;
+}
+
+void PrintDoubleArray(int[,]array)  // Вывод двумерного массива
+{
+    for(int i = 0; i < array.GetLength(0); i++) 
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j]} ");
+            Console.WriteLine(); 
+        } 
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillDoubleArray(row, column, -10, 10);
+PrintDoubleArray(array);
