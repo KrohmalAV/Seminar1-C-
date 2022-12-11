39738d4fc6c27c5b420872b909f70efe590447d4
KrohmalAV <Anton.avk@yandex.ru>
2022-12-11 10:56:29 +0300

    Создание двумерного массива с заполнением через сложение индексов

---
+++ Sem7.2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 48: Задайте двумерный массив размера m на n,
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+
+int[,] FillDoubleArray(int row, int column)  // Заполнение двумерного массива (рандомно)
+{
+    int[,] array = new int[row, column];
+    for(int i = 0; i < row; i++) 
+        for(int j = 0; j < column; j++)
+            array[i,j] = i + j;
+    return array;
+}
+
+void PrintDoubleArray(int[,]array)  // Вывод двумерного массива
+{
+    for(int i = 0; i < array.GetLength(0); i++) 
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j]} ");
+            Console.WriteLine(); 
+        } 
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillDoubleArray(row, column);
+PrintDoubleArray(array);
