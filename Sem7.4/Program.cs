fcdc6ea9a2b7da6d89c43bd24a6e43424feee2b6
KrohmalAV <Anton.avk@yandex.ru>
2022-12-11 11:20:44 +0300

    Нахождение суммы элементов двумерного массива по главной диагонали

---
+++ Sem7.4/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 51: Задайте двумерный массив. Найдите сумму элементов,
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+
+int[,] FillDoubleArray(int row, int column, int min, int max)  // Заполнение двумерного массива (рандомно)
+{
+    int[,] array = new int[row, column];
+    for(int i = 0; i < row; i++) 
+        for(int j = 0; j < column; j++)
+            array[i,j] = new Random().Next(min, max);
+    return array;
+}
+
+void PrintDoubleArray(int[,]array)  // Вывод двумерного массива
+{
+    for(int i = 0; i < array.GetLength(0); i++) 
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j]} ");
+            Console.WriteLine(); 
+        } 
+}
+
+int SumMainElementsArray(int[,]array)  // Нахождение суммы элементов на главной диагонали
+{
+    int total = 0;
+    for(int i = 0; i < array.GetLength(0); i++) 
+        for(int j = 0; j < array.GetLength(1); j++)
+            if(i == j)
+                total += array[i,j];
+    return total;
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillDoubleArray(row, column, 0, 10);
+PrintDoubleArray(array);
+Console.WriteLine($"Сумма элементов главной диагонали: {SumMainElementsArray(array)}");
+
